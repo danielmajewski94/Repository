04b3377b3410603b30f15bcb3618c56c91eb5844
danielmajewski94 <daniel.majewski@iu-study.org>
2024-01-17 15:19:34 +0100

    Styleanpassungen und Grid-Anpassungen

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ var mediaQuery = window.matchMedia("(max-width: 450px)");
 
 function handleMediaChange() {
 
-    const Portraitimage = document.getElementById('Portraitimage');
+    //const Portraitimage = document.getElementById('Portraitimage');
     const Mail = document.getElementById('Mail');
     const Adresse = document.getElementById('Adresse');
 
     if (mediaQuery.matches) {
-        Portraitimage.style.display = "none";
+        //Portraitimage.style.display = "none";
         Mail.style.display = "none";
         Adresse.style.display = "none";
     } else {
-        Portraitimage.style.display = "inline";
+        //Portraitimage.style.display = "inline";
         Mail.style.display = "inline";
         Adresse.style.display = "inline";
     }
