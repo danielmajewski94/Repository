38d3ab4081db7cab5c5e555138d81bd1ba770126
danielmajewski94 <daniel.majewski@iu-study.org>
2024-01-17 14:41:12 +0100

    Überarbeitungen am Layout

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ document.addEventListener('DOMContentLoaded', function () {
         menu.classList.toggle('show');
     });
 });
+
+var mediaQuery = window.matchMedia("(max-width: 450px)");
+
+function handleMediaChange() {
+
+    const Portraitimage = document.getElementById('Portraitimage');
+    const Mail = document.getElementById('Mail');
+    const Adresse = document.getElementById('Adresse');
+
+    if (mediaQuery.matches) {
+        Portraitimage.style.display = "none";
+        Mail.style.display = "none";
+        Adresse.style.display = "none";
+    } else {
+        Portraitimage.style.display = "inline";
+        Mail.style.display = "inline";
+        Adresse.style.display = "inline";
+    }
+}
+
+mediaQuery.addListener(handleMediaChange);
